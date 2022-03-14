2656ebe8fc18f78fb9d38ebde53721033df13757
Dmitry <radiator211@mail.ru>
2022-03-14 20:10:28 +0700

    Initial Commit

---
+++ Freedom/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Freedom
+//
+//  Created by Дмитрий Бессонов on 14.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
