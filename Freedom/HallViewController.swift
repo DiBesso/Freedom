affc75980033100e28a31e0121b0a696dbd10079
Dmitry <radiator211@mail.ru>
2022-03-19 20:00:09 +0700

    Work with navigation controller and backgroundColor

--- Freedom/HallViewController.swift
+++ Freedom/HallViewController.swift
@@ -14,11 +14,14 @@ class HallViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.addSubview(hallCollectionView)
+        title = "Freedom"
+        view.backgroundColor = .init(red: 0.5176, green: 0.5176, blue: 0.5098, alpha: 1)
         
+        view.addSubview(hallCollectionView)
+
         hallCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         hallCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        hallCollectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 150).isActive = true
+        hallCollectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 130).isActive = true
         hallCollectionView.heightAnchor.constraint(equalToConstant: 500).isActive = true
         
         
