61975ef1865354c89da8ba82672f1629c58a9251
Dmitry <radiator211@mail.ru>
2022-04-29 23:51:14 +0700

    Add buttons: telegram, vk

--- Freedom/ContactsViewController.swift
+++ Freedom/ContactsViewController.swift
@@ -10,25 +10,43 @@ import UIKit
 class ContactsViewController: UIViewController, UITextViewDelegate {
 
     
-    @IBOutlet weak var waButton: UIButton!
-    
+    @IBOutlet weak var whatsAppButton: UIButton!
+    @IBOutlet weak var telegramButton: UIButton!
+    @IBOutlet weak var vkButton: UIButton!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .init(red: 0.5176, green: 0.5176, blue: 0.5098, alpha: 1)
-
+        setButtons()
     }
+
     
-    @IBAction func waAction(_ sender: UIButton) {
+    @IBAction func whatsAppAction(_ sender: UIButton) {
         
         guard let url = URL(string: "https://wa.me/+79833042806") else { return }
         UIApplication.shared.open(url, options: [:], completionHandler: nil)
     }
     
-    func hyperLink(originalText: String, hyperLink: String, url: String) {
-
+    @IBAction func telegramAction(_ sender: UIButton) {
+        guard let url = URL(string: "https://t.me/freedom_photostudio_nsk") else { return }
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
     }
     
+    @IBAction func vkAction(_ sender: UIButton) {
+        guard let url = URL(string: "https://vk.com/freedom_photostudio") else { return }
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+    }
+    
+    func setButtons() {
+        whatsAppButton.titleLabel?.font = UIFont(name: "Copperplate Bold", size: 28)
+        telegramButton.titleLabel?.text = "Telegram"
+        telegramButton.titleLabel?.font = UIFont(name: "Copperplate Bold", size: 28)
+        telegramButton.titleLabel?.textColor = .white
+        vkButton.titleLabel?.text = "VK"
+        vkButton.titleLabel?.font = UIFont(name: "Copperplate Bold", size: 28)
+        vkButton.titleLabel?.textColor = .white
+        
+    }
 }
