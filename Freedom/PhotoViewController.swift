021279661f27d99b8425e97be4bd4055cc5b4a72
Dmitry <radiator211@mail.ru>
2022-07-30 23:50:51 +0700

    Setting shareAction

--- Freedom/PhotoViewController.swift
+++ Freedom/PhotoViewController.swift
@@ -8,19 +8,28 @@
 import UIKit
 
 class PhotoViewController: UIViewController {
-
+    
     @IBOutlet weak var photoImageView: UIImageView!
     var image: UIImage?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         view.backgroundColor = .black
         
         photoImageView.image = image
     }
     
     @IBAction func shareAction(_ sender: Any) {
+        let shareController = UIActivityViewController(activityItems: [image!], applicationActivities: nil)
+        
+        shareController.completionWithItemsHandler = { _, bool, _, _ in
+            if bool {
+                print("Успешно!")
+            }
+        }
+        
+        present(shareController, animated: true, completion: nil)
     }
     
     
